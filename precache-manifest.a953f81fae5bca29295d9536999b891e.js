0ee2a695bfe1b69c8d3c9a2cd7b3dadb5f905b06
Adam Butterworth <adam@casey-2.local>
2019-01-10 20:05:26 -0500

    Updates

---
+++ precache-manifest.a953f81fae5bca29295d9536999b891e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "27f6ee2d928e96647af2",
+    "url": "/static/css/main.19378e47.chunk.css"
+  },
+  {
+    "revision": "27f6ee2d928e96647af2",
+    "url": "/static/js/main.27f6ee2d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4858e857324404cd4597426544f96f87",
+    "url": "/index.html"
+  }
+];
